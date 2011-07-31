d5ef898b7201595958cefb6ffadbfa48d1c95214
Ramana <sramana9@gmail.com>
2011-07-31 19:48:53 +0530

    Fixing reset in demo

--- demo/extra_controller.py
+++ demo/extra_controller.py
@@ -1,10 +1,10 @@
 
 def reset():
-    import random
     from gluon.contrib.populate import populate
     for table in tables:
         db[table].truncate()
         if table not in auth_tables:
-            populate(db[table],random.randint(10, 100))
+            populate(db[table],100)
+            db.commit()
 
     redirect(URL('welcome'))
