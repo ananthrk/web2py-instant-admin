f930c003196178645fe4655a416f25f75acc37b3
Ramana <sramana9@gmail.com>
2011-08-03 15:14:00 +0530

    Adding Date, Time & DateTime widgets. Fixing hide sidebar issue

--- static/plugin_instant_admin/javascripts/application.js
+++ static/plugin_instant_admin/javascripts/application.js
@@ -2,6 +2,32 @@ if (typeof($j) === "undefined" && typeof(jQuery) !== "undefined") {
   var $j = jQuery.noConflict();
 }
 
+
+function hideSidebar()
+{
+    $j('#sidebar').hide();
+    $j('#main').width('100%');
+    $j('#toggle_img').attr("src", $j("#show-icon")[0].href);
+    $j('#toggle_img').attr("title", "Show Sidebar");
+    $j.cookie('hide-sidebar', 'yes', { expires: 30, path: '/'  });
+}
+
+function showSidebar()
+{
+    $j('#sidebar').show();
+    $j('#main').width('77%');
+    $j('#toggle_img').attr("src", $j("#hide-icon")[0].href);
+    $j('#toggle_img').attr("title", "Hide Sidebar");
+    $j.cookie('hide-sidebar', null, { expires: 30, path: '/'  });
+}
+
+function toggle_sidebar() {
+  if( $j("#sidebar").is(":visible") )
+      hideSidebar();
+  else
+      showSidebar();
+}
+
 $j(document).ready(function($){
   // accordeon
   $("#nav .more:not(.active) ul").hide();
@@ -32,4 +58,35 @@ $j(document).ready(function($){
     });
   });
 
+  // Hide Flash messages after 10 seconds.
+  $('.flash').delay(10000).fadeOut();
+
+  // Toggle sidebar
+  //$('#toggle').toggle(showSidebar, hideSidebar);
+  if ( $.cookie('hide-sidebar') == null )
+  {
+    showSidebar();
+  }
+  else
+  {
+    hideSidebar();
+  }
+
+  // Widgets for Date, Time and DateTime fields
+  $(".date").datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dateFormat: "yy-mm-dd"
+  });
+  $(".time").timepicker({
+      ampm: true,
+      timeFormat: "hh:mm:ss"
+  });
+  $(".datetime").datetimepicker({
+      changeMonth: true,
+      changeYear: true,
+      dateFormat: "yy-mm-dd",
+      ampm: true,
+      timeFormat: "hh:mm:ss"
+  });
 });
